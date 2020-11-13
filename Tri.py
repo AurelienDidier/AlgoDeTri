63bc2601691ba1f76dd60a6ae5656302aa80f1c9
GAME1.1 <GAME1.1@etpa.local>
2020-11-13 12:49:02 +0100

    Implémentation du tri fusion et du tri par sélection à compléter

--- Tri.py
+++ Tri.py
@@ -1,1 +1,35 @@
+def selectMin(list):
+    var=list[0]
+    for i in list:
+        if var>i:
+            var=i
+    return var
 
+def triSelect (list):
+    result=[]
+    while len(list)>0:
+        var=selectMin(list)
+        """
+        A compléter
+        """
+    return result
+
+print(selectMin([4,2,7,8]))
+
+def triFusion(list):
+        if len(list)>0:
+            """
+            A compléter
+            list0= partie gauche de la liste
+            list1= partie droite de la liste
+            
+            list0Trie = triFusion(list0)
+            list1Trie = triFusion(list1)
+            return fusion (list0Trie,list1Trie)
+            """
+        else return list    
+    
+def fusion(list):
+    """
+    A compléter
+    """
